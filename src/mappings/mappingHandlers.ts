cacde6328897c6e0d85671b7201589e27d4e7243
VargSupercolony <varg.vikernes@supercolony.net>
2021-11-25 15:11:35 +0200

    add explicit id set

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -5,6 +5,7 @@ import {ContractEvent} from "../types";
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
     //Retrieve the record by its ID
     const record = await ContractEvent.get(event.extrinsic.block.block.header.hash.toString());
+    record.id = event.extrinsic.block.block.header.hash.toString();
     record.eventJson = JSON.stringify(event.extrinsic.events.map((e) => e.event), null, 2)
     await record.save();
 }
